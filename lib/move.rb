dd92a59e83fe5afbf0091d3b107b1e7ce5a5074e
Deepak Barwal <deepakbarwal98@gmail.com>
2019-03-01 13:40:20 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,5 +1,3 @@
-board = []
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -10,5 +8,6 @@ end
 
 # code your input_to_index and move method here!
 def input_to_index(input)
+  input = gets.strip
   board[] << "input".to_i
 end
